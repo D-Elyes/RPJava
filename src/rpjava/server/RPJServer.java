c3476740e61b08138ad3360c8b3555391106c894
FlorentBerland <florent.berland@laposte.net>
2018-03-12 17:07:11 +0100

    add : server with code for login

---
+++ src/rpjava/server/RPJServer.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rpjava.server;
+
+import DerbyJavaDb.*;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import lib.ocsf.server.*;
+import rpjava.common.*;
+import rpjava.common.exception.*;
+
+/**
+ *
+ * @author Florent BERLAND
+ */
+public class RPJServer extends AbstractServer {
+
+    AccountDAO accountDAO;
+    
+    public RPJServer(int port, AccountDAO accountDAO) {
+        super(port);
+        this.accountDAO = accountDAO;
+    }
+
+    @Override
+    protected void handleMessageFromClient(Object msg, ConnectionToClient client) {
+        if(msg instanceof Account){
+            User userConnected = null; // Get user from database
+            try{
+                if (userConnected != null){
+                    client.sendToClient(userConnected);
+                } else {
+                    client.sendToClient(new InvalidAccountException());
+                }
+            } catch (IOException e){
+                
+            }
+        }
+    }
+    
+     public static void main(String[] args){
+        RPJServer server = new RPJServer(12345, null);
+        try {
+            server.listen();
+        } catch (IOException ex) {
+            Logger.getLogger(RPJServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
