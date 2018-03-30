57f9a0aab5524bc2e35212b54653af15fa93b9f9
Unknown <matthieudye@yahoo.fr>
2018-03-30 11:51:40 +0200

    Auto stash before merge of "florent" and "origin/florent"

--- src/rpjava/common/Item.java
+++ src/rpjava/common/Item.java
@@ -6,30 +6,60 @@
 package rpjava.common;
 
 /**
- *
- * @author Florent BERLAND
+ * This class gathers all informations about items
+ * @author RPJavaTeam
  */
 public class Item {
+
+// INSTANCE PROPERTIES --------------------------------------------------------
     
+    /**
+    * Property that will contain the name and the description of an item
+    */
     private String name, description;
 
+// CONSTRUCTOR -----------------------------------------------------------------
+    
+    /**
+    * This method creates an Item instance
+    * @param name The name of the item
+    * @param description The description of the item
+    */
     public Item(String name, String description) {
         this.name = name;
         this.description = description;
     }
 
+// GETTERS AND SETTERS ---------------------------------------------------------
+    
+    /**
+    * This method gets the name of the item
+    * @return String The name of the inventory
+    */   
     public String getName() {
         return name;
     }
 
+    /**
+    * This method change the name of the item
+    * @param name The new name of the item
+    */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+    * This method gets the description of the item
+    * @return String The description of the item
+    */  
     public String getDescription() {
         return description;
     }
 
+    /**
+    * This method change the description of the item
+    * @param description The new description of the item
+    */
     public void setDescription(String description) {
         this.description = description;
     }
